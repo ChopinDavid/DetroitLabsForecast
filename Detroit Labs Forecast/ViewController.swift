8179b51337b7c2996bfd3deebfc0519f9d50e4b0
David Chopin <dchopin1@live.maryville.edu>
2020-04-14 11:54:20 -0500

    Initial Commit

---
+++ Detroit Labs Forecast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Detroit Labs Forecast
+//
+//  Created by David G Chopin on 4/14/20.
+//  Copyright © 2020 All Caps Software Incorporated. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
